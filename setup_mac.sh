e99c915e76414fb917d3d9995cc34ac910faf07b
Christopher <kitizz.devside@gmail.com>
2023-06-05 16:01:24 +1200

    Add oh-my-zsh plugins support

--- setup_mac.sh
+++ setup_mac.sh
@@ -9,7 +9,7 @@ then
 fi
 
 # Install the starship prompt.
-brew install starship || true
+HOMEBREW_NO_AUTO_UPDATE=1 brew install starship || true
 
 # Load iTerm colors.
 mkdir -p $HOME/Documents/iterm_settings
@@ -18,3 +18,13 @@ cp -i com.googlecode.iterm2.plist $HOME/Documents/iterm_settings/com.googlecode.
 
 # Install Oh-My-Zsh if not already installed.
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" || true
+
+# Read Oh-my-zsh plugins from a specific file
+sed -i '' -e 's/^plugins=.*/zmodload zsh\/mapfile;plugins=( "${(f)mapfile[myrc\/zsh_plugins]}" )/g' $HOME/.zshrc
+
+# Add conda completions to plugins.
+CONDA_COMPL_DIR = ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/conda-zsh-completion
+if [ ! -d $CONDA_COMPL_DIR ]
+then
+    git clone https://github.com/esc/conda-zsh-completion $CONDA_COMPL_DIR
+fi
